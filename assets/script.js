1ebd26014dd7baee6f86d07db36c9d7cf4b08d26
Mathys Cogne Foucault <mathys.cognef@gmail.com>
2023-03-19 18:58:26 +0100

    clear code / init function gen dots

--- assets/script.js
+++ assets/script.js
@@ -93,105 +93,13 @@ arrowSlide.forEach(button => {
 	});
   });
 
+function dots(){
+	// Génération des dots
+	const dots = document.createElement('i');
+	dots.classList.add('dot');
+	description.innerHTML = slides[0].tagLine;
+	divSlide.appendChild(description);
+};
+
 // Lancer a la géneration de la page
 startSlide();
-
-
-
-
-
-// arrowLeft.addEventListener("click", function () {
-// 	// console.log("arrowLeft");
-// 	slide(-1);
-// });
-
-// arrowRight.addEventListener("click", function () {
-// 	// console.log("arrowRight");
-// 	slide(+1)
-// });
-
-// function slide(n){
-// 	moveSlide=n;
-// 	console.log("Prev Slide");
-// 	console.log(moveSlide);
-
-// 	const slideNumbers = slides.length -1
-
-// 	if (indexSlide < 0){
-// 		indexSlide = slideNumbers;
-// 		console.log("call fonction DEBUT INDEX SLIDE" + indexSlide);
-// 		moveSlide(indexSlide)
-// 	}
-// 	if (indexSlide >= slideNumbers){
-// 		indexSlide = -1;
-// 		console.log("call fonction FIN INDEX SLIDE" + indexSlide);
-// 		moveSlide(indexSlide)
-// 	}
-
-// 	changeSlide();
-// }
-
-
-// changeSlide()
-
-// function changeSlide(){
-// 	// console.log(">>>>>>>> la slide " +indexSlide)
-
-// }
-
-// function TESTmoveSlide(n){
-
-// 	if (n === 1){
-// 		indexSlide++;
-// 		console.log(">>>>>>>> la slide " +indexSlide)
-// 	}
-
-// 	if (n === -1){
-// 		indexSlide--;
-// 		console.log(">>>>>>>> la slide " +indexSlide)
-// 	}
-// }
-	
-
-// Maintenant que nous écoutons le clic sur les flèches, passons à un autre élément de l’interface : les bullets points. Ils permettront à l’utilisateur de savoir sur quelle slide il se trouve.
-
-// Ajouter les bullet points sur la partie basse du slider. Il nous faut un point par image contenue dans le slider. 
-// Une fois les points affichés, différencier le point qui signale la diapositive en cours de visionnage : ce sera la première. Dans le CSS, le point en cours a une classe spécifique.
-// Recommandations : 
-
-// Pour savoir combien de points mettre, on peut compter, via JavaScript, le nombre d’éléments présents dans le tableau “slides” du fichier `script.js` , et afficher un point par image. 
-// Pour comprendre comment ajouter les points, vous pouvez vous référer au fichier CSS du site, il vous donnera les informations nécessaires sur les classes à utiliser pour les différents types de points. 
-
-
-// for (let i = 0; i < slides.length; i++) {
-// 	const slideArborescence + i = slides[i].image;
-// 	console.log(slideArborescence)
-
-
-// 	const slideTexte = slides[i].tagLine;
-// 	console.log(slideTexte)
-
-
-	// // Selection d'ou vas l'img
-	// const sectionSlide = document.querySelector(".banner");
-	// // Crea de la balise img
-	// const imageElement = document.createElement("img");
-	// // Ajout d'une classe
-	// imageElement.classList.add("banner-img")
-	// // Ajout du src de l'img
-	// imageElement.src = slideArborescence;
-	// // Indiquer ou vas l'img
-	// sectionSlide.appendChild(imageElement);
-
-
-// }
-
-
-// arrowRight.addEventListener("click", function () {
-// 	console.log("==== Right TEST ====");
-// 	const sectionSlide = document.querySelector(".banner");
-// 	const imageElement = document.createElement("img");
-// 	imageElement.classList.add("banner-img")
-// 	imageElement.src = slideArborescence;
-// 	sectionSlide.appendChild(imageElement);
-// });

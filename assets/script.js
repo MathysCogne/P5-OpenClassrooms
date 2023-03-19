0583031cec536189023d40f89b71ebac548e5a97
Mathys Cogne Foucault <mathys.cognef@gmail.com>
2023-03-19 19:31:42 +0100

    Fonction init dots done

--- assets/script.js
+++ assets/script.js
@@ -16,73 +16,86 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
-
+// Lancer a la géneration de la page
+initSlide();
+initDots()
 // variables: Selection banniere / Fleches / Index banniere
 const divSlide = document.querySelector(".banner");
+const divDots = document.querySelector(".dots");
 const arrowSlide = document.querySelectorAll(".arrow");
 let indexSlide = 0;
 
 // Reset de la banniere
 function resetDivSlide() {
-	divSlide.innerHTML = '';
+	divSlide.innerHTML = "";
 }
 
+
 // Init du slider
-function startSlide() {
+function initSlide() {
 	resetDivSlide();
-	// Génération de l'image
-	const img = document.createElement('img');
+
+	// Génération de l"image
+	const img = document.createElement("img");
 	img.src = slides[0].image;
-	img.classList.add('banner-img');
+	img.classList.add("banner-img");
 	divSlide.appendChild(img);
 
 	// Génération du texte
-	const description = document.createElement('p');
+	const description = document.createElement("p");
 	description.innerHTML = slides[0].tagLine;
 	divSlide.appendChild(description);
+	console.log("DEBUG: Init slider");
 }
 
 // slider gauche
 function slideLeft() {
 	resetDivSlide();
-	const img = document.createElement('img');
+
+	const img = document.createElement("img");
 	img.src = slides[indexSlide - 1].image;
-	img.classList.add('banner-img');
+	img.classList.add("banner-img");
 	divSlide.appendChild(img);
 
-	const description = document.createElement('p');
+	const description = document.createElement("p");
 	description.innerHTML = slides[indexSlide - 1].tagLine;
 	divSlide.appendChild(description);
 
 	indexSlide--;
+	initDots()
+
+	console.log("DEBUG: slide >>> " + indexSlide);
 }
 
 // slider droite
 function slideRight() {
 	resetDivSlide();
-	const img = document.createElement('img');
+
+	const img = document.createElement("img");
 	img.src = slides[indexSlide + 1].image;
-	img.classList.add('banner-img');
+	img.classList.add("banner-img");
 	divSlide.appendChild(img);
 
-	const description = document.createElement('p');
+	const description = document.createElement("p");
 	description.innerHTML = slides[indexSlide + 1].tagLine;
 	divSlide.appendChild(description);
 
 	indexSlide++;
+	initDots()
+
+	console.log("DEBUG: slide >>> " + indexSlide);
 }
 
 arrowSlide.forEach(button => {
-	button.addEventListener('click', () => {
+	button.addEventListener("click", () => {
 
-	  if (button.classList.contains('arrow_left')) {
+	  if (button.classList.contains("arrow_left")) {
 		// Première diapositive  > dernière
 		if (indexSlide === 0) {
 			indexSlide = slides.length;
 		}
 		slideLeft();
 
-		
 	  } else {
 		// Dernière diapositive > première
 		if (indexSlide === slides.length - 1) {
@@ -93,13 +106,20 @@ arrowSlide.forEach(button => {
 	});
   });
 
-function dots(){
+function initDots(){
+	divDots.innerHTML = "";
 	// Génération des dots
-	const dots = document.createElement('i');
-	dots.classList.add('dot');
-	description.innerHTML = slides[0].tagLine;
-	divSlide.appendChild(description);
-};
-
-// Lancer a la géneration de la page
-startSlide();
+	for (let i = 0; i < slides.length; i++) {
+		if (indexSlide === i){
+			const dots = document.createElement("i");
+			dots.classList.add("dot");
+			dots.classList.add("dot_selected");
+			divDots.appendChild(dots);
+			console.clear();
+			console.log("DEBUG: dot >>> " + i);
+		} else {
+			const dots = document.createElement("i");
+			dots.classList.add("dot");
+			divDots.appendChild(dots);
+		};		
+}};
